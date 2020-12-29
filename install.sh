69633598f07e1b1dcac016327ee004647ddc485b
dbabojelic <dario.bab27@gmail.com>
2020-12-29 16:47:25 +0100

    Add vimrc and tmux.conf

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
 git submodule update --init --recursive
-# fzf additional
+
+# zsh  
 ~/dotfiles/zsh/plugins/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish &> /dev/null
 rm -f ~/dotfiles/zsh/.fzf.zsh
 mv ~/.fzf.zsh ~/dotfiles/zsh
 
 rm -f ~/.zshrc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+
+# vim
+rm -rf ~/.vim
+ln -sf ~/dotfiles/vim ~/.vim
+rm -f ~/.vimrc
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+
+#tmux
+rm -f ~/.tmux.conf
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
