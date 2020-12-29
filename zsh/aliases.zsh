d308c362d04bfd8c05171e87e3eb86db86cc8da0
dbabojelic <dario.bab27@gmail.com>
2020-12-29 12:00:36 +0100

    Removed local aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,8 +5,3 @@ alias vimconf="vim ~/.vimrc"
 alias bashconf="vim ~/.bash_profile"
 alias gs="git status"
 alias tmuxconf="vim ~/.tmux.conf"
-
-# OPTIMO specific
-alias db_access="$HOME/workspace/devops/docker/tools/db_access.sh"
-alias getim="$HOME/workspace/devops/docker/tools/get_imports.sh"
-alias oip="$HOME/workspace/optimo/venv/bin/ipython"
