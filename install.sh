1248ebfc7591b24e0e56b1628abfeb545006dc85
dbabojelic <dario.bab27@gmail.com>
2020-12-29 17:43:48 +0100

    Add partial install and gitconfig

--- install.sh
+++ install.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 git submodule update --init --recursive
 
-# zsh  
-~/dotfiles/zsh/plugins/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish &> /dev/null
-rm -f ~/dotfiles/zsh/.fzf.zsh
-mv ~/.fzf.zsh ~/dotfiles/zsh
+function zsh {
+  ~/dotfiles/zsh/plugins/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish &> /dev/null
+  rm -f ~/dotfiles/zsh/.fzf.zsh
+  mv ~/.fzf.zsh ~/dotfiles/zsh
+  rm -f ~/.zshrc
+  ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+}
 
-rm -f ~/.zshrc
-ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+function vim {
+  rm -rf ~/.vim
+  ln -sf ~/dotfiles/vim ~/.vim
+  rm -f ~/.vimrc
+  ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+}
 
-# vim
-rm -rf ~/.vim
-ln -sf ~/dotfiles/vim ~/.vim
-rm -f ~/.vimrc
-ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+function tmux {
+  rm -f ~/.tmux.conf
+  ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+}
 
-#tmux
-rm -f ~/.tmux.conf
-ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+function git {
+  rm -f ~/.gitconfig
+  ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
+}
+
+for i in "$@"
+do
+    case $i in
+        all)
+            zsh
+            vim
+            tmux
+            git
+            ;;
+        zsh)
+            zsh
+            ;;
+        vim)
+            vim
+            ;;
+        tmux)
+            tmux
+            ;;
+        git)
+            git
+            ;;
+    esac
+done
